ea6026559db3520a3511d74b861f5277c0b5314e
Joshua Barrass <josh.barrass.work@gmail.com>
2022-06-11 11:56:08 +0100

    Scale colour values in difference shader
    This makes the values more like those from the old code, which will
    ease comparing the two results. Since GL_RGBA32F is being used,
    clipping is no longer a concern.

--- shaders/difference_shader.glsl
+++ shaders/difference_shader.glsl
@@ -8,8 +8,8 @@ uniform vec2 screen_size;
 
 void main() {
      vec2 uv = (gl_FragCoord.xy) / screen_size;
-     vec3 targetColor = texture(target, uv).xyz;
-     vec3 canvasColor = texture(canvas, uv).xyz;
+     vec3 targetColor = texture(target, uv).xyz * 255.;
+     vec3 canvasColor = texture(canvas, uv).xyz * 255.;
      vec3 diff = canvasColor - targetColor;
      TexColor = vec4(dot(diff, diff), 0.0, 0.0, 1.0);
 }
