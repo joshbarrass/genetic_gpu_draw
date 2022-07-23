a785f265fd8a21d3bfd0d307adb7b0ecd28c8eed
Joshua Barrass <josh.barrass.work@gmail.com>
2022-07-23 17:35:43 +0100

    Add SVG output to the command line arguments

--- src/parse_args.cpp
+++ src/parse_args.cpp
@@ -10,29 +10,32 @@ Usage:
 
 -f --target          Path to image file to aim for
 -o --output          Path to output final image to
+-v --svg             Path to output SVG to
 -n --triangles       Number of triangles to draw
 -g --per-generation  Number of triangles to attempt to draw each generation
+-s --seed            Set the seed used to generate the triangles
 -r --resume          Path to image to load instead of blank canvas
 -h --help            Print this message
 )";
 
 int Main::parseArgs(int argc, char **argv) {
   this_program = std::string(argv[0]);
-  
-  option longopts[] = {
-      {"triangles", required_argument, NULL, 'n'},
-      {"output", required_argument, NULL, 'o'},
-      {"target", required_argument, NULL, 'f'},
-      {"per-generation", required_argument, NULL, 'g'},
-      {"resume", required_argument, NULL, 'r'},
-      {"seed", required_argument, NULL, 's'}, {"help", no_argument, NULL, 'h'},
-      {0}};
+
+  option longopts[] = {{"triangles", required_argument, NULL, 'n'},
+                       {"output", required_argument, NULL, 'o'},
+                       {"target", required_argument, NULL, 'f'},
+                       {"per-generation", required_argument, NULL, 'g'},
+                       {"resume", required_argument, NULL, 'r'},
+                       {"seed", required_argument, NULL, 's'},
+                       {"help", no_argument, NULL, 'h'},
+                       {"svg", required_argument, NULL, 'v'},
+                       {0}};
 
   int opt;
   // drop into new scope because this_file may never be initialised
   // and is not needed beyond this
   {std::filesystem::path this_file;
-  while ((opt = getopt_long(argc, argv, "n:o:f:g:r:s:h", longopts, 0)) != -1) {
+  while ((opt = getopt_long(argc, argv, "n:o:f:g:r:s:v:h", longopts, 0)) != -1) {
     switch (opt) {
     case 'h':
       this_file = std::filesystem::path(argv[0]);
@@ -61,6 +64,10 @@ int Main::parseArgs(int argc, char **argv) {
     case 's':
       SEED = atoi(optarg);
       set_seed = true;
+      break;
+    case 'v':
+      SVG_FILE = std::string(optarg);
+      set_svg_file = true;
       break;
     case ':':
       using namespace std;
